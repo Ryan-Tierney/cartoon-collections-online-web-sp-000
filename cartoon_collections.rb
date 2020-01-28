c4a8b18e27c59c2dd165046f7b8e0b7de35f9f63
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-01-28 07:52:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,14 @@ def summon_captain_planet(array)
     planeteer_calls
   end 
 
-def long_planeteer_calls
+def long_planeteer_calls(array)
+  if array.length < 4 
+    true 
+  else 
+    false 
+  end 
+end 
+  
 end
 
 def find_the_cheese# code an argument here
